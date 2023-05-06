9d0f0a8ef3167b04742d0200d5e2b74a342416b1
Anne Edmondson <edmondson.anne@gmail.com>
2023-05-05 21:14:58 -0600

    Commit week 5 assignment in progress - js file coded out - need to debug.

--- menuApp.js
+++ menuApp.js
@@ -1,7 +1,3 @@
-// Use at least one array.
-// Use at least two classes.
-// Options to create, view, delete elements 
-
 class AesSedai {
     constructor(name, title, nationality, ajah) {
         this.name = name
@@ -18,94 +14,133 @@ class AesSedai {
 class Ajah {
     constructor(color) {
         this.color = color
-        this.ajahMembers = []
+        this.aesSedais = []
     }
 
-    createAesSedai(ajahMember) {
-        if (ajahMember instanceof AesSedai) {
-            this.ajahMembers.push(ajahMember)
+    addAesSedai(aesSedai) {
+        if (aesSedai instanceof AesSedai) {
+            this.aesSedais.push(aesSedai)
         } else {
-            throw new Error(`You may only add an Aes Sedai to an Ajah. This ajahMember is not an Aes Sedai. `)
+            throw new Error(`You may only create a new Aes Sedai. ${aesSedai} can not channel the One Source.`)
         }
     }
 
     describe() {
-        return `${this.ajahMembers.length} belong to the ${this.color} Ajah.`
+        return `${this.aesSedais.length} belong to the ${this.color} Ajah.`
     }
 } 
 
 class MenuApp {
-    constructor(ajahs, chosenAjah) {
+    constructor() {
         this.ajahs = []
         this.chosenAjah = null
     }
 
     start() {
         let choice = this.showMainMenuChoices()
+        
         while (choice != 0) {
             switch (choice) {
-                case 'a':
-                    this.createAjah()
+                case '1':
+                    this.createAnAjah()
                     break
-                case 'b':
+                case '2':
                     this.deleteAnAjah()
                     break
-                case 'c':
-                    this.viewAnAjah
+                case '3':
+                    this.viewAnAjah()
                     break
-                case 'd':
-                    this.viewAllAjahs
+                case '4':
+                    this.viewAllAjahs()
                     break
                 default:
-                    choice = 'e'
+                    choice = 0
             }
             choice = this.showMainMenuChoices()
         }
-        alert('Go with the Light!')
+    alert('Go with the Light!')
     }
 
-    showAjahMenuChoices() {
+    showMainMenuChoices() {
         return prompt(`
-            a. Create a New Ajah
-            b. Delete an Ajah
-            c. View an Ajah
-            d. View All Ajahs
-            e. Exit Application
+            1. Create a New Ajah
+            2. Delete an Ajah
+            3. View an Ajah
+            4. View All Ajahs
+            0. Exit Application
         `)
     }
 
     showAjahMenuChoices(ajahInfo) {
         return prompt(`
-            a. Create a New Aes Sedai
-            b. Delete an Aes Sedai
-            c. Return to Main Menu
+            1. Create a New Aes Sedai
+            2. Delete an Aes Sedai
+            0. Return to Main Menu
             _________________________
 
-            ${this.ajahInfo}
+            ${ajahInfo}
         `)
     }
 
-    createANewAjah() {
-        let color = prompt('Enter the Color of Your New Ajah: ')
+    createAnAjah() {
+        let color = prompt('Enter the color of a new Ajah: ')
         this.ajahs.push(new Ajah(color))
     }
     
     deleteAnAjah () {
-
+        let index = prompt (`Enter the index of the Ajah you wish to delete: `)   
     }
 
     viewAnAjah() {
-
+        let index = prompt(`Enter the index of the Ajah that you want to view: `)
+        if (index > -1 && index < this.ajahs.length) {
+            this.chosenAjah = this.ajahs[index]
+            
+            let ajahDescription = 'Ajah Color: ' + this.chosenAjah.color + '\n'
+            
+            ajahDescription += ' ' + this.chosenAjah.describe() + '\n '            
+            
+            for (let i = 0; i < this.chosenAjah.ajahs.length; i++) {
+                ajahDescription += i + ') ' + this.chosenAjah.aesSedais[i].describe() + '\n';
+            }
+            
+            let choice1 = this.showAesSedaiMenuOptions(ajahDescription);
+            switch (choice1) {
+                case '1' :
+                this.createAesSedai();
+                break;
+                case '2' :
+                this.deleteAesSedai();
+            }
+        }  // validate user input
     }
     
     viewAllAjahs() {
-        let ajahName = ''
+        let ajahColor = ''
         
         for (let i = 0; i < this.ajahs.length; i ++) {
-            ajahName += i + `) ` + this.ajahs[i].name + '\n'
+            ajahColor += i + `) ` + this.ajahs[i].color + '\n'
         }
-        alert(ajahName)
+        
+        alert(ajahColor)
     }
 
-    createAesSedai
-} 
+    createAesSedai() {
+        let name = prompt(`Enter the name of your new Aes Sedai:`)
+        let title = prompt(`Enter the title of your new Aes Sedai: `)
+        let nationality = prompt(`Enter the nationality of your new Aes Sedai: `)
+        let ajah = prompt(`Enter the Ajah your new Aes Sedai belongs to: `)
+
+        this.chosenAjah.addAesSedai(new AesSedai(name, title, nationality, ajah))
+    }
+
+    deleteAesSedai() {
+        let index = prompt(`Enter the index of the Aes Sedai you would like to delete: `)
+
+        if (index > -1 && index < this.chosenAjah.aesSedais.length) {
+            this.chosenAjah.aesSedais.splice(index, 1)
+        }
+    }
+}
+let menu = new MenuApp()
+menu.start()
