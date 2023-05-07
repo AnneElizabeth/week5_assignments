a47fbd5025d552065b762919d33e43c94eba52c6
Anne Edmondson <edmondson.anne@gmail.com>
2023-05-06 23:00:07 -0700

    Commit updated week 5 assignment. View an Ajah not working.

--- menuApp.js
+++ menuApp.js
@@ -1,146 +1,169 @@
+// Anne Edmondson
+// 2023-03-30-fe-mountain
+
+/* I'm currently obsessed with the Wheel of Time fantasy series and am currently reading book 4 of 14. Please bear with my tie-in to the series. If you haven't read the books, an "Aes Sedai" is a type of witch and an "Ajah" is like the sorority or coven she belongs to. Each Ajah has a different color theme and a different mission in the series.*/
+
+// class created as blueprint for new Aes Sedai
+
 class AesSedai {
-    constructor(name, title, nationality, ajah) {
+    constructor(name, ajah) {
         this.name = name
-        this.title = title
         this.nationality = nationality
-        this.ajah = ajah
     }
     
     describe() {
-        return `${this.title} ${this.name} Sedai is a member of the ${this.ajah}. She is ${this.nationality}.`
+        return (`${this.name} Sedai's nationality is ${this.nationality}.`)
     }
 }
+// class created as blueprint for new Ajah
 
 class Ajah {
-    constructor(color) {
-        this.color = color
-        this.aesSedais = []
+    constructor(name) {
+        this.name = name
+        this.aesSedais = [] 
     }
+
+// method for adding a new Aes Sedai, throws error if user entry is not an instance of the AesSedai class
 
     addAesSedai(aesSedai) {
         if (aesSedai instanceof AesSedai) {
             this.aesSedais.push(aesSedai)
         } else {
-            throw new Error(`You may only create a new Aes Sedai. ${aesSedai} can not channel the One Source.`)
+            throw new Error(`You may only add a new Aes Sedai. ${aesSedai} can not channel the One Source.`)
         }
     }
 
     describe() {
-        return `${this.aesSedais.length} belong to the ${this.color} Ajah.`
+        return `The ${this.name} Ajah has ${this.aesSedais.length} Aes Sedai members.`
     }
 } 
 
-class MenuApp {
+// this class drives the application, contains the core features and methods of the application.
+
+class Menu {
     constructor() {
         this.ajahs = []
         this.chosenAjah = null
     }
+// method that starts application
+// code below follows the top-down development approach - build methods first then code them out with desired functionality
 
     start() {
-        let choice = this.showMainMenuChoices()
-        
-        while (choice != 0) {
-            switch (choice) {
+        let option = this.showMainMenu()
+ 
+        // this method retrieves user choice/entry and executes appropriate method based on their choice
+
+        while (option != 0) {
+            switch (option) {
                 case '1':
-                    this.createAnAjah()
+                    this.createAjah()
                     break
                 case '2':
-                    this.deleteAnAjah()
+                    this.deleteAjah()
                     break
                 case '3':
-                    this.viewAnAjah()
+                    this.viewAjah()
                     break
                 case '4':
                     this.viewAllAjahs()
                     break
                 default:
-                    choice = 0
+                    option = 0
             }
-            choice = this.showMainMenuChoices()
+            option = this.showMainMenu()
         }
-    alert('Go with the Light!')
+        alert(`Go with the Light!` )
     }
+// modal that displays user choices
 
-    showMainMenuChoices() {
+    showMainMenu() {
         return prompt(`
+            0. Exit
+            -------------------
             1. Create a New Ajah
             2. Delete an Ajah
             3. View an Ajah
             4. View All Ajahs
-            0. Exit Application
         `)
     }
 
-    showAjahMenuChoices(ajahInfo) {
+    showSubMenu () {
         return prompt(`
+            0. Return to Main Menu
+            ----------------------    
             1. Create a New Aes Sedai
             2. Delete an Aes Sedai
-            0. Return to Main Menu
-            _________________________
-
-            ${ajahInfo}
+            ----------------------
+            
         `)
     }
 
-    createAnAjah() {
-        let color = prompt('Enter the color of a new Ajah: ')
-        this.ajahs.push(new Ajah(color))
-    }
-    
-    deleteAnAjah () {
-        let index = prompt (`Enter the index of the Ajah you wish to delete: `)   
+ // method for creating new Ajah   
+    createAjah() {
+        let name = prompt('Enter the color of a new Ajah: ')
+        this.ajahs.push(new Ajah(name))
     }
 
-    viewAnAjah() {
-        let index = prompt(`Enter the index of the Ajah that you want to view: `)
-        if (index > -1 && index < this.ajahs.length) {
-            this.chosenAjah = this.ajahs[index]
+    //method for viewing details of each Ajah
+    viewAjah() {
+        let i = prompt('Enter the index of the Ajah you want to view: ')
+
+        // user input validation
+        if (i > -1 && i < this.ajahs.length) {
+            this.chosenAjah = this.ajahs[i]
+            let description = 'Ajah Color: ' + this.chosenAjah.name + '\n'
             
-            let ajahDescription = 'Ajah Color: ' + this.chosenAjah.color + '\n'
-            
-            ajahDescription += ' ' + this.chosenAjah.describe() + '\n '            
+ //   build list of all Aes Sedai           
             
             for (let i = 0; i < this.chosenAjah.ajahs.length; i++) {
-                ajahDescription += i + ') ' + this.chosenAjah.aesSedais[i].describe() + '\n';
+                description += i + ') ' + this.chosenAjah.aesSedais[i].name + ' - ' + this.chosenAjah.aesSedais[i].nationality + '\n'
             }
-            
-            let choice1 = this.showAesSedaiMenuOptions(ajahDescription);
-            switch (choice1) {
+ // submenu of full menu           
+            let option = this.showSubMenu(description)
+            switch (option) {
                 case '1' :
-                this.createAesSedai();
-                break;
+                this.createAesSedai()
+                break
                 case '2' :
-                this.deleteAesSedai();
+                this.deleteAesSedai()
             }
-        }  // validate user input
+        }
     }
-    
-    viewAllAjahs() {
-        let ajahColor = ''
+ 
+  // iterates through array of Ajahs, displays name/color of each one
+    viewAllAjahs() {   
+        let ajahNames = ' '
         
         for (let i = 0; i < this.ajahs.length; i ++) {
-            ajahColor += i + `) ` + this.ajahs[i].color + '\n'
+            ajahNames += i + `) ` + this.ajahs[i].name + '\n'
+        }       
+        alert(ajahNames)
+    }
+
+    deleteAjah() {
+        let i = prompt(`Enter the index of the Ajah to delete: `)
+
+        // user input validation
+        if (i > -1 && i < this.ajahs.length) {
+            this.ajahs.splice(i, 1)
         }
-        
-        alert(ajahColor)
     }
 
     createAesSedai() {
-        let name = prompt(`Enter the name of your new Aes Sedai:`)
-        let title = prompt(`Enter the title of your new Aes Sedai: `)
+        let name = prompt(`Enter the name of a new Aes Sedai:`)
         let nationality = prompt(`Enter the nationality of your new Aes Sedai: `)
-        let ajah = prompt(`Enter the Ajah your new Aes Sedai belongs to: `)
 
-        this.chosenAjah.addAesSedai(new AesSedai(name, title, nationality, ajah))
+        this.chosenAjah.aesSedais.push(new AesSedai(name, nationality))
     }
 
     deleteAesSedai() {
-        let index = prompt(`Enter the index of the Aes Sedai you would like to delete: `)
+        let i = prompt(`Enter the index of the Aes Sedai to delete: `)
 
-        if (index > -1 && index < this.chosenAjah.aesSedais.length) {
-            this.chosenAjah.aesSedais.splice(index, 1)
+        // user input validation
+        if (i > -1 && i < this.chosenAjah.aesSedais.length) {
+            this.chosenAjah.aesSedais.splice(i, 1)
         }
     }
 }
-let menu = new MenuApp()
+let menu = new Menu()
 menu.start()
